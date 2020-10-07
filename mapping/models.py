a36d0f1d58c6f76515353ba7c1991b266ba3c62a
rasu204 <59288730+rasu204@users.noreply.github.com>
2020-10-07 15:58:48 +0530

    Update models.py

--- mapping/models.py
+++ mapping/models.py
@@ -5,7 +5,7 @@ from django.contrib.postgres.fields import HStoreField
 # Create your models here.
 class Mapping(models.Model):
     # MappingID = models.IntegerField(primary_key = True)
-    UserID = models.ForeignKey('user.User', on_delete=models.RESTRICT)
+    #UserID = models.ForeignKey('user.User', on_delete=models.RESTRICT)
     MappingFor = models.TextField(null=True)
-    CreatedOn = models.DateField(auto_now_add=True, null=True)
+    #CreatedOn = models.DateField(auto_now_add=True, null=True)
     Mappings = HStoreField(null=True)
