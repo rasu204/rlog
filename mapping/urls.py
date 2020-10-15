acc88ac12a21d618a049fa3bc305edddb300dab1
rasu204 <59288730+rasu204@users.noreply.github.com>
2020-10-15 19:54:30 +0530

    Update urls.py

--- mapping/urls.py
+++ mapping/urls.py
@@ -5,7 +5,6 @@ urlpatterns = [
         path('import', views.import_data),
         path('fieldmatching', views.fieldmatching),
         path('choose', views.choose),
-        path('', views.disp, name='mapping'),
         path('import_p', views.import_data_p),
         # path('', views.MappingView.as_view(), name = 'mapping')
 
